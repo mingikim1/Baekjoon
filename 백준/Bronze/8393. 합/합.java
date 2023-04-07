abfc395709dbc55ffc8224c18ba5d2a22da438b4
minggi777 <129666585+minggi777@users.noreply.github.com>
2023-04-07 16:54:39 +0900

    [Bronze V] Title: 합, Time: 212 ms, Memory: 17640 KB -BaekjoonHub

---
+++ 백준/Bronze/8393. 합/합.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+	
+	public static void main(String[] args) {	
+		
+		Scanner sc = new Scanner (System.in);
+		
+		int num = sc.nextInt();
+		
+		int i;
+			
+		int sum = 0;
+		
+		for (i=1 ; i <= num ; i++) {
+			
+			sum += i;
+		}
+		System.out.println(sum);
+	}
+}
