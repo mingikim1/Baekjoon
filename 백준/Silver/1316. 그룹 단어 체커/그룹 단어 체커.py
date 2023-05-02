2435e7b2ae195f4409afc31bf3fef9e49a35d160
minggi777 <129666585+minggi777@users.noreply.github.com>
2023-05-02 17:06:11 +0900

    [Silver V] Title: 그룹 단어 체커, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.py
+++ 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.py
@@ -1,26 +1,12 @@
-import sys
-
 N = int(input())
-num_array = []
-counting = 0
-for i in range(N) :
-    S = sys.stdin.readline().strip()
-    S_set = set(S)
-    for j in S_set :
-        num_array = []
-        for k in range(len(S)) :
-            if S[k] == j :                
-                num_array.append(k)
-                conti = True
-                if len(num_array) == 1 :
-                    continue
-        for l in range(len(num_array) - 1) :
-            if num_array[l+1]-num_array[l] != 1 :
-                conti = False
-                break
-        if conti == False :
-            break
-    if conti == True :
-        counting = counting + 1
+counting = N
 
+for i in range(N) :
+    S = input()
+    for j in range(len(S)-1) :
+        if S[j] == S[j+1] :
+            pass
+        elif S[j] in S[j+1::] :
+            counting += -1
+            break
 print(counting)
